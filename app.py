53f99e5451f09216aa2073c68268d9efa9ac6677
psps17 <priyaps1599@gmail.com>
2025-09-24 00:17:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-print("Hello World")
+from flask import Flask, render_template
+
+# Create a Flask application instance
+app = Flask(__name__)
+
+# Define a route for the home page
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# Run the application if this file is executed directly
+if __name__ == '__main__':
+    app.run(debug=True)
