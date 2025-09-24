68f1247648acef7f8b68da2f7b58055f3d5e02d5
Priya <priyaps1599@gmail.com>
2025-09-24 22:56:07 +0530

    port chhck

--- app.py
+++ app.py
@@ -1,17 +1,16 @@
-import os
 from flask import Flask
+
 
 # Create a Flask application instance
 app = Flask(__name__)
+
 
 # Define a route for the home page
 @app.route('/')
 def home():
     return 'Hello World'
 
-# Get port from environment variable, default to 8080 if not set
-port = int(os.environ.get("PORT", 8080))
 
-# Run the application
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=port, debug=True)
+# Run the application if this file is executed directly
+if __name__ == '__main__':
+   app.run(host='0.0.0.0', port=8080,debug=True)
