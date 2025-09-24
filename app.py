d817d779a0a25878b6a35163c1d909ad0dee4866
psps17 <priyaps1599@gmail.com>
2025-09-24 20:56:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,4 +10,4 @@ def home():
 
 # Run the application if this file is executed directly
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', debug=True)
